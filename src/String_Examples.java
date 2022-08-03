26a94f1b18138e5607cb9821c712d94cd4b49ee4
Ksenia Evsjuk <k.evsjuk@gmail.com>
2022-08-03 20:56:41 +0300

    String thingy

--- src/String_Examples.java
+++ src/String_Examples.java
@@ -1,5 +1,68 @@
 public class String_Examples {
     public static void main(String[] args) {
-        
+       String text1 = "This is text example";
+
+       String text2 = text1.intern();
+
+       System.out.println(text2);
+
+       //CONCATENATION OF STRINGS
+
+        String input1 = "This is ";
+        String input2 = "input text.";
+
+        String inputCombined = input1.concat(input2);
+        System.out.println(inputCombined);
+
+        //COMPARING STRINGS
+
+        String val1 = "Test";
+        String val2 = "Test";
+        System.out.println(val1.equals(val2));
+
+        //STRING CLASS METHODS
+
+        String inputText = "This is my text";
+
+        //Length
+        System.out.println(inputText.length());
+
+        //To uppercase
+        System.out.println(inputText.toUpperCase());
+        System.out.println(inputText);
+
+        //To lowercase
+        System.out.println(inputText.toLowerCase());
+
+        //STRING CLASS METHOD - INDEXOF()
+        String speakingText = "This is test value";
+        System.out.println(speakingText.indexOf('e'));
+        System.out.println(speakingText.indexOf(" is "));
+
+        //REPLACEALL()
+        //Sudas
+
+        String textForTV = "This phone is complete sudas. And this case also is sudas.";
+        System.out.println(textForTV.replaceAll("sudas", "s***s"));
+
+        //Character index
+        System.out.println(textForTV.charAt(textForTV.length()-1));
+
+        //Substring
+        System.out.println(textForTV.substring(5));
+        System.out.println(textForTV.substring(5,12));
+
+        //Parsing
+        //Converting String to primitive Data types
+
+        String intValue = "165";
+        String floatValue = "2.0";
+        String doubleValue = "true";
+        String longValue = "100000000000000";
+
+        System.out.println(Integer.parseInt(intValue)+4);
+        System.out.println(Float.parseFloat(floatValue));
+        System.out.println(Boolean.parseBoolean(doubleValue));
+        System.out.println(Long.parseLong(longValue));
     }
 }
