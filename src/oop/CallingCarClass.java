2b048ba78f5c5896e34091f2e9a452c71693e58b
Ksenia Evsjuk <k.evsjuk@gmail.com>
2022-08-19 19:05:47 +0300

    Before missed HWs and not done properly

---
+++ src/oop/CallingCarClass.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class CallingCarClass {
+    public static void main(String[] args) {
+
+        //Create a new object from Car class template
+        //Set field values
+        //Call printCarInfo method to check field values
+
+        Car car1 = new Car();
+        //car1.brand = "BMW";
+        //car1.maxSpeed = "208";
+        //car1.color = "Black";
+
+        car1.setBrand(BMW);
+        car1.maxSpeed = "208";
+        car1.color = "Black";
+    }
+}
---
+++ src/oop/Car.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class Car {
+
+    //Fields
+    private String color;
+    String brand;
+    int maxSpeed;
+
+    //Methods
+
+    public void printCarInfo(){
+        System.out.println("oop.Car info: ");
+        System.out.println("Brand: " + brand);
+        System.out.println("Color: "+ color);
+        System.out.println("Maximum speed: " + maxSpeed);
+
+    }
+}
